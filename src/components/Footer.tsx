4f15ee2e742c41e7f8f22ff6f2e345c8e581d629
PereZzZzZ580 <114631210+PereZzZzZ580@users.noreply.github.com>
2025-08-16 13:39:52 -0500

    en el footer ya se incluye la seccion de empresa, soporte, legal, contacto y redes sociales con los enlaces a cada uno de los componentes. se añade nueva seccion de contacto con direccion, correo y telefono, se crearon paginas de terminos y condiciones, politica de provacidad y politica de cookies con contenido inicial

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
 import { Facebook, Instagram } from "lucide-react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-bg">
       <div className="container py-10 md:py-14 lg:py-18">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 text-white/85">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8 text-white/85">
           <div className="min-w-[220px] max-w-[260px]">
             <h3 className="text-gold text-base md:text-lg font-semibold mb-4">Empresa</h3>
             <ul className="flex flex-col gap-2">
@@ -49,10 +49,24 @@ export default function Footer() {
             </ul>
           </div>
           <div className="min-w-[220px] max-w-[260px]">
+            <h3 className="text-gold text-base md:text-lg font-semibold mb-4">Contacto</h3>
+            <ul className="flex flex-col gap-2">
+              <li>Calle 63 #17-45, Bogotá, Colombia</li>
+              <li>
+                <a href="mailto:contacto@hhbarberhome.com" className="hover:text-gold">
+                  contacto@hhbarberhome.com
+                </a>
+              </li>
+              <li>
+                <a href="tel:3138907119" className="hover:text-gold">3138907119</a>
+              </li>
+            </ul>
+          </div>
+          <div className="min-w-[220px] max-w-[260px]">
             <h3 className="text-gold text-base md:text-lg font-semibold mb-4">Síguenos</h3>
             <div className="flex gap-4">
               <Link
-                href="https://instagram.com"
+                href="https://www.instagram.com/hhbarberhome/"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Instagram"
@@ -61,11 +75,11 @@ export default function Footer() {
                 <Instagram size={24} />
               </Link>
               <Link
-                href="https://facebook.com"
+                href="https://www.facebook.com/hhbarberhome"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Facebook"
-                  className="text-gold hover:text-gold-600"
+                className="text-gold hover:text-gold-600"
               >
                 <Facebook size={24} />
               </Link>
