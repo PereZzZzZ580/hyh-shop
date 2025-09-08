8a2b70b722dc139cd75d9b7b572d45e23e641a6b
PereZzZzZ580 <juandras2004perez@gmail.com>
2025-09-08 17:34:09 -0500

    modificacion de favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,17 @@ export const metadata: Metadata = {
   description: "Ropa y productos para barbería",
   icons: {
     icon: [
-      { url: "/favico_barberia.ico", type: "image/x-icon", sizes: "64x64" },
       { url: "/favico_barberia-32.png", type: "image/png", sizes: "32x32" },
       { url: "/favico_barberia-64.png", type: "image/png", sizes: "64x64" },
+      { url: "/favico_barberia.ico", type: "image/x-icon" },
     ],
+    shortcut: "/favico_barberia-64.png",
   },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <html lang="es" className="bg-bg text-text">
+    <html lang="es" className="bg-bg text-text">
       <body>
         <Header />
         <main className="pt-20 mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
